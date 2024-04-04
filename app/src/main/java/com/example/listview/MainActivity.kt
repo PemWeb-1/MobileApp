19d7f82957edc8321685f147a6e4e3fe52dc35be
Varatlan <148843870+Varatlan@users.noreply.github.com>
2024-04-04 23:21:58 +0700

    menghapus comment pada main Activity

--- app/src/main/java/com/example/listview/MainActivity.kt
+++ app/src/main/java/com/example/listview/MainActivity.kt
@@ -11,7 +11,6 @@ import com.example.listview.R.id.lView
 import com.example.listview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-//    private  var listItems = ArrayList<String>()
 
     lateinit var binding: ActivityMainBinding
 
@@ -20,14 +19,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        setContentView(R.layout.activity_main)
 
         val bengkel = arrayOf("honda, sidoarjo", "honda, surabaya", "yamaha, sidoarjo", "yamaha, surabaya", "mitsubishi, sidoarjo", "mitsubishi, surabaya", "ford, surabaya", "mustang, surabaya", "nasa, surabaya", "tesla, surabaya")
 
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1,
             bengkel)
-//        val listView = findViewById<ListView>(R.id.lView)
-//        listView.adapter = adapter
+
 
         binding.lView.adapter = adapter
 
@@ -48,15 +45,5 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-//        val button = findViewById<Button>(R.id.button)
-//        button.setOnClickListener {
-//            val editText = findViewById<EditText>(R.id.editText)
-//            val text = editText.text.toString()
-//            if(text.isNotEmpty()) {
-//                listItems.add(text)
-//                adapter.notifyDataSetChanged()
-//                editText.text.clear()
-//            }
-//        }
     }
 }
